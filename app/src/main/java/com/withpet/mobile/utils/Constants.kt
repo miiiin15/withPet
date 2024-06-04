a96841d564b98f1ae13fdbdf86012e4e31ee26e8
MintaeKim <gg04253@gmail.com>
2024-06-04 17:36:35 +0900

    Feat: 유틸 추가

---
+++ app/src/main/java/com/withpet/mobile/utils/Constants.kt
@@ -0,0 +1,16 @@
+package com.withpet.mobile.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+@SuppressLint("StaticFieldLeak")
+object Constants {
+
+    var context: Context? = null
+
+//    운영서버
+
+    //개발서버
+    const val SERVER_URL = "http://210.115.230.154:59020/"
+
+}
---
+++ app/src/main/java/com/withpet/mobile/utils/Logcat.kt
@@ -0,0 +1,93 @@
+package com.withpet.mobile.utils
+
+import android.os.Environment
+import android.util.Log
+import java.io.*
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class Logcat {
+    companion object {
+        // 로그의 태그로 사용될 상수
+        private const val TAG: String = "위드팻 로그캣"
+
+        // 디버그 로그 출력
+        fun d(message: String) {
+            Log.d(TAG, buildLogMessage(message))
+        }
+
+        // 에러 로그 출력
+        fun e(message: String) {
+            Log.e(TAG, buildLogMessage(message))
+        }
+
+        // 정보 로그 출력
+        fun i(message: String) {
+            Log.i(TAG, buildLogMessage(message))
+        }
+
+        // 경고 로그 출력
+        fun w(message: String, exception: Exception? = null) {
+            Log.w(TAG, buildLogMessage(message))
+            exception?.let { Log.w(TAG, it) }
+        }
+
+        // 상세 로그 출력
+        fun v(message: String) {
+            Log.v(TAG, buildLogMessage(message))
+        }
+
+        // 로그 메시지 구성
+        private fun buildLogMessage(message: String): String {
+            val ste = Thread.currentThread().stackTrace[4]
+            val sb = StringBuilder()
+
+            sb.append("[")
+            sb.append(ste.fileName)
+            sb.append("] ")
+            sb.append(ste.methodName)
+            sb.append(" #")
+            sb.append(ste.lineNumber)
+            sb.append(": ")
+            sb.append(message)
+
+            // writeLog(sb.toString())
+            return sb.toString()
+        }
+
+        // 로그를 파일로 저장
+        private fun writeLog(str: String) {
+            var result = 0
+            val dirPath = Environment.getExternalStorageDirectory().absolutePath + "/log/$TAG/"
+            val file = File(dirPath)
+
+            if (!file.exists())
+                file.mkdirs()
+
+            val nowDate =
+                SimpleDateFormat("yyyy-MM-dd").format(Date(System.currentTimeMillis()))
+
+            val savefile = File("$dirPath$nowDate.txt")
+            try {
+                val nowTime =
+                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date(System.currentTimeMillis()))
+
+                val bfw = BufferedWriter(FileWriter("$dirPath$nowDate.txt", true))
+                bfw.write("++ Time: $nowTime\n")
+                bfw.write(str)
+                bfw.write("\n")
+                bfw.flush()
+                bfw.close()
+            } catch (e: FileNotFoundException) {
+                result = 1
+                e.printStackTrace()
+            } catch (e: IOException) {
+                result = 1
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
