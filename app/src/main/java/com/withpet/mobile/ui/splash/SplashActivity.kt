546bd88eae9a17228793cf7a322b492ecc436b21
mintae <gg04253@gmail.com>
2024-06-04 23:32:16 +0900

    Feat: 버전 조회 api 연결

--- app/src/main/java/com/withpet/mobile/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/withpet/mobile/ui/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.withpet.mobile.R
 import com.withpet.mobile.data.repository.CommonRepo
@@ -18,13 +19,31 @@ class SplashActivity : AppCompatActivity() {
         checkPermissionsAndVersion()
 
         // 초기화 완료 후 로그인 화면으로 전환
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }, 2000) // 2초 후 전환
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, LoginActivity::class.java))
+//            finish()
+//        }, 2000) // 2초 후 전환
     }
 
     private fun checkPermissionsAndVersion() {
         // 권한 및 버전 확인 로직
+        CommonRepo.getVersion(
+            success = {
+                if (it.result.code == 200) {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                    Toast.makeText(this, "최신버전은 : ${it.payload.version} 입니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "${it.error?.message.toString()}", Toast.LENGTH_SHORT).show()
+                }
+
+            },
+            networkFail = {
+                Toast.makeText(this, "네트웍실패", Toast.LENGTH_SHORT).show()
+            },
+            failure = {
+                Toast.makeText(this, "그냥실패", Toast.LENGTH_SHORT).show()
+            }
+        )
     }
 }
