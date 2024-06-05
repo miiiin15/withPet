d65efd84d1441e34372628a9c551c92693b8e155
MintaeKim <gg04253@gmail.com>
2024-06-05 11:30:22 +0900

    Feat: 검사로직 추가

--- app/src/main/java/com/withpet/mobile/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/withpet/mobile/ui/splash/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.withpet.mobile.ui.splash
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.withpet.mobile.R
@@ -11,32 +15,29 @@ import com.withpet.mobile.data.repository.CommonRepo
 import com.withpet.mobile.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
+    companion object {
+        private const val PERMISSION_REQUEST_ID = 11
+        private var requestPermissionGranted = false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // 초기화 로직 수행
-        checkPermissionsAndVersion()
-
-        // 초기화 완료 후 로그인 화면으로 전환
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            startActivity(Intent(this, LoginActivity::class.java))
-//            finish()
-//        }, 2000) // 2초 후 전환
+        initPermissions()
     }
 
-    private fun checkPermissionsAndVersion() {
-        // 권한 및 버전 확인 로직
+    private fun checkVersionAndNavigate() {
         CommonRepo.getVersion(
             success = {
                 if (it.result.code == 200) {
                     startActivity(Intent(this, LoginActivity::class.java))
                     finish()
-                    Toast.makeText(this, "최신버전은 : ${it.payload.version} 입니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "${it.payload}", Toast.LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(this, "${it.error?.message.toString()}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "${it.error?.message.toString()}", Toast.LENGTH_SHORT)
+                        .show()
                 }
-
             },
             networkFail = {
                 Toast.makeText(this, "네트웍실패", Toast.LENGTH_SHORT).show()
@@ -46,4 +47,54 @@ class SplashActivity : AppCompatActivity() {
             }
         )
     }
+
+    private fun initPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permissionCamera = checkSelfPermission(Manifest.permission.CAMERA)
+            val permissionWriteStorage =
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            val permissionLocation = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+            if (permissionCamera == PackageManager.PERMISSION_DENIED ||
+                permissionWriteStorage == PackageManager.PERMISSION_DENIED ||
+                permissionLocation == PackageManager.PERMISSION_DENIED
+            ) {
+                val permissions = arrayOf(
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                requestPermissions(permissions, PERMISSION_REQUEST_ID)
+            } else {
+                requestPermissionGranted = true
+                checkVersionAndNavigate()
+            }
+        } else {
+            Log.e("권한", "PermissionGrant run with old way")
+            requestPermissionGranted = true
+            checkVersionAndNavigate()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_ID) {
+            requestPermissionGranted =
+                grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }
+            if (requestPermissionGranted) {
+                checkVersionAndNavigate()
+            } else {
+                Toast.makeText(this, "권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+
