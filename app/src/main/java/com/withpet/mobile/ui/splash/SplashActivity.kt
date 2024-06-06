1e7bea2bb64ff0a6541401189c236c8c56ac88f7
mintae <gg04253@gmail.com>
2024-06-06 23:35:29 +0900

    Feat: 자동 로그인 구현

--- app/src/main/java/com/withpet/mobile/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/withpet/mobile/ui/splash/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.withpet.mobile.ui.splash
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -10,8 +12,10 @@ import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.withpet.mobile.MainActivity
 import com.withpet.mobile.R
 import com.withpet.mobile.data.repository.CommonRepo
+import com.withpet.mobile.data.repository.SignInRepo
 import com.withpet.mobile.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -20,20 +24,24 @@ class SplashActivity : AppCompatActivity() {
         private var requestPermissionGranted = false
     }
 
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        initPermissions()
+        sharedPreferences = getSharedPreferences("userPreferences", Context.MODE_PRIVATE)
+
+        // 버전을 확인하고 이후 sharedPreferences를 검사하고 로직을 진행
+        checkVersionAndNavigate()
     }
 
     private fun checkVersionAndNavigate() {
         CommonRepo.getVersion(
             success = {
                 if (it.result.code == 200) {
-                    startActivity(Intent(this, LoginActivity::class.java))
-                    finish()
-                    Toast.makeText(this, "${it.payload}", Toast.LENGTH_SHORT).show()
+                    // sharedPreferences를 검사하고 이후 로직을 진행
+                    checkSharedPreferences()
                 } else {
                     Toast.makeText(this, "${it.error?.message.toString()}", Toast.LENGTH_SHORT)
                         .show()
@@ -46,6 +54,17 @@ class SplashActivity : AppCompatActivity() {
                 Toast.makeText(this, "그냥실패", Toast.LENGTH_SHORT).show()
             }
         )
+    }
+
+    private fun checkSharedPreferences() {
+        // 로그인 정보가 존재하는지 확인하고, 있으면 자동 로그인 시도
+        val loginId = sharedPreferences.getString("loginId", "")
+        val password = sharedPreferences.getString("password", "")
+        if (!loginId.isNullOrBlank() && !password.isNullOrBlank()) {
+            signIn(loginId, password)
+        } else {
+            initPermissions()
+        }
     }
 
     private fun initPermissions() {
@@ -71,12 +90,12 @@ class SplashActivity : AppCompatActivity() {
                 requestPermissions(permissions, PERMISSION_REQUEST_ID)
             } else {
                 requestPermissionGranted = true
-                checkVersionAndNavigate()
+                navigateToLogin()
             }
         } else {
             Log.e("권한", "PermissionGrant run with old way")
             requestPermissionGranted = true
-            checkVersionAndNavigate()
+            navigateToLogin()
         }
     }
 
@@ -90,11 +109,41 @@ class SplashActivity : AppCompatActivity() {
             requestPermissionGranted =
                 grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }
             if (requestPermissionGranted) {
-                checkVersionAndNavigate()
+                // 권한이 획득되었으면 sharedPreferences를 검사하고 이후 로직을 진행
+                checkSharedPreferences()
             } else {
                 Toast.makeText(this, "권한이 필요합니다.", Toast.LENGTH_SHORT).show()
             }
         }
     }
-}
 
+    private fun signIn(loginId: String, password: String) {
+        SignInRepo.signIn(
+            loginId = loginId,
+            password = password,
+            networkFail = {
+                Toast.makeText(this, "네트워크 실패: $it", Toast.LENGTH_SHORT).show()
+            },
+            success = {
+                if (it.result.code == 200) {
+                    navigateToMainActivity()
+                } else {
+                    Toast.makeText(this, "실패: ${it.result.message}", Toast.LENGTH_SHORT).show()
+                }
+            },
+            failure = {
+                Toast.makeText(this, "에러: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+
+    private fun navigateToLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+    private fun navigateToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
