e333f99e62f26ce905351e06c516f93f82a63cdc
mintae <gg04253@gmail.com>
2024-06-10 00:24:22 +0900

    Rename: 폴더 구조 변경

--- app/src/main/java/com/withpet/mobile/ui/fragment/SecondFragment.kt
+++ app/src/main/java/com/withpet/mobile/ui/fragment/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.withpet.mobile
+package com.withpet.mobile.ui.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.withpet.mobile.R
 import com.withpet.mobile.databinding.FragmentSecondBinding
 
 /**
