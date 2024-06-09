0cb6b757a816837da3d0328e7285399a8cf1a9ed
mintae <gg04253@gmail.com>
2024-06-09 19:56:35 +0900

    Feat: 커스텀버튼 추가

---
+++ app/src/main/java/com/withpet/mobile/ui/custom/CustomButton.kt
@@ -0,0 +1,39 @@
+package com.withpet.mobile.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.withpet.mobile.R
+
+
+class CustomButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = android.R.attr.buttonStyle
+) : AppCompatButton(context, attrs, defStyleAttr) {
+
+    init {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        // 속성 초기화
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.CustomButton)
+            val textValue = typedArray.getString(R.styleable.CustomButton_android_text)
+            typedArray.recycle()
+
+            // 텍스트 설정
+            text = textValue ?: "확인"
+        }
+
+        background = ContextCompat.getDrawable(context, R.drawable.custom_button_selector)
+        setTextColor(ContextCompat.getColor(context, R.color.white))
+    }
+
+    // isEnable 속성을 설정하는 함수
+    fun setEnable(isEnabled: Boolean) {
+        this.isEnabled = isEnabled
+    }
+}
