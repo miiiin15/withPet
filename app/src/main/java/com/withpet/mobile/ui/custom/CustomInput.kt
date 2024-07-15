3413330bd6b29514dc371e02ea14628802a937a3
MintaeKim <gg04253@gmail.com>
2024-07-15 18:14:18 +0900

    Feat: CustomInput 밑줄 수정

--- app/src/main/java/com/withpet/mobile/ui/custom/CustomInput.kt
+++ app/src/main/java/com/withpet/mobile/ui/custom/CustomInput.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.LayerDrawable
 import android.util.AttributeSet
 import android.util.TypedValue
+import android.view.Gravity
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
 import com.withpet.mobile.R
@@ -13,111 +14,100 @@ interface IsValidListener {
     fun isValid(text: String): Boolean
 }
 
-class CustomInput : AppCompatEditText {
-
-    private var isDisabled: Boolean = false
-
-    constructor(context: Context) : super(context) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-        init()
-    }
-
-
-    private fun init() {
-        // EditText의 배경을 투명한 밑줄로 설정
-        background = ContextCompat.getDrawable(context, R.drawable.custom_input_bg)
-        // inputType이 textpassword 타입이 아닌경우 1줄만
-        isSingleLine = inputType != 129 && inputType != 128
-
-        setTextAppearance(context, R.style.CustomInputTextStyle)
-        setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
-        setHintTextColor(ContextCompat.getColor(context,R.color.disable))
-
-        // 입력 중, 올바른 값, 올바르지 않은 값에 따라 밑줄 색상을 변경
-        setOnFocusChangeListener { _, hasFocus ->
-            val colorResId = if (hasFocus) {
-
-                R.color.primary // 기본 값
-            } else if (isValid() && !hasFocus) {
-                R.color.disable // 기본 값
-            } else if (isValid() && hasFocus) {
-                R.color.primary // 올바른 값
-            } else {
-                R.color.error // 올바르지 않은 값
-            }
-            setUnderlineColor(ContextCompat.getColor(context, colorResId))
-        }
-    }
-
-    private fun setUnderlineColor(color: Int) {
-        val layerDrawable = background as? LayerDrawable
-        layerDrawable?.findDrawableByLayerId(R.id.underLine)?.apply {
-            if (this is GradientDrawable) {
-                setColor(color)
-            }
-        }
-    }
-
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        // 텍스트가 변경될 때 수행할 작업
-        val text = s.toString()
-        val colorResId = if (isValid(text)) {
-            if (hasFocus()) {
-                R.color.primary // 올바른 값
-            } else {
-                R.color.disable // 올바른 값
-            }
-        } else {
-            R.color.disable // 올바르지 않은 값
-        }
-        setUnderlineColor(ContextCompat.getColor(context, colorResId))
-    }
-
-//    }
+class CustomInput @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatEditText(context, attrs, defStyleAttr) {
 
     private var isValidListener: IsValidListener? = null
+
+    init {
+        setBackgroundResource(R.drawable.custom_input_bg)
+        setPadding(0, 20, 0, 20)
+        gravity = Gravity.START or Gravity.CENTER_VERTICAL
+        setHintTextColor(ContextCompat.getColor(context, R.color.disable))
+        setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+
+        isFocusable = true
+        isFocusableInTouchMode = true
+
+        setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                setUnderlineColor(R.color.primary)
+            } else {
+                if (!isValid()) {
+                    setUnderlineColor(R.color.error)
+                } else {
+                    setUnderlineColor(R.color.disable)
+                }
+            }
+        }
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        val colorResId = if (enabled) R.color.disable else R.color.disable
+        setUnderlineColor(colorResId)
+
+        val colorResIdBackground = if (enabled) R.color.transparent else R.color.disable
+        setCustomBackgroundColor(colorResIdBackground)
+
+        val hintTextColorResId = if (enabled) R.color.disable else R.color.txt1
+        setHintTextColor(ContextCompat.getColor(context, hintTextColorResId))
+    }
+    override fun setError(error: CharSequence?) {
+        super.setError(error)
+        if (error != null) {
+            setUnderlineColor(R.color.error)
+        } else {
+            setUnderlineColor(R.color.disable)
+        }
+    }
+
+    fun setDisable(disable: Boolean) {
+        isEnabled = !disable
+    }
 
     fun setIsValidListener(listener: IsValidListener) {
         isValidListener = listener
     }
 
-    // 포커스용
     private fun isValid(): Boolean {
         val text = text.toString()
-        return isValidListener?.isValid(text) ?: text.isNotEmpty() // listener가 설정되지 않으면 항상 true로 가정
+        return isValidListener?.isValid(text) ?: text.isNotEmpty()
     }
 
-    //값 변화 검사용
-    private fun isValid(text: String): Boolean {
-        return isValidListener?.isValid(text) ?: text.isNotEmpty() // listener가 설정되지 않으면 항상 true로 가정
-    }
-
-    override fun setEnabled(enabled: Boolean) {
-        super.setEnabled(enabled)
-        // disable 상태일 경우 전체 입력 필드의 색상 변경
-        val colorResId = if (enabled) {
-            R.color.txt1 // 정상 상태 색상
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        val text = s.toString()
+        if (isValid(text)) {
+            setUnderlineColor(R.color.primary)
         } else {
-            R.color.txt3 // 비활성화 상태 색상
+            setUnderlineColor(R.color.error)
         }
-        setHintTextColor(ContextCompat.getColor(context, colorResId))
-        isDisabled = !enabled // 상태 저장
-        setUnderlineColor(ContextCompat.getColor(context, R.color.disable))
     }
 
-    fun setDisable(disable: Boolean) {
-        isDisabled = disable
-        setEnabled(!disable)
+    private fun isValid(text: String): Boolean {
+        return isValidListener?.isValid(text) ?: text.isNotEmpty()
+    }
+
+    private fun setUnderlineColor(colorResId: Int) {
+        val drawable = background
+        if (drawable is LayerDrawable) {
+            val underline = drawable.findDrawableByLayerId(R.id.underLine) as? GradientDrawable
+            underline?.setColor(ContextCompat.getColor(context, colorResId))
+        } else {
+            background?.mutate()?.setTint(ContextCompat.getColor(context, colorResId))
+        }
+    }
+
+    private fun setCustomBackgroundColor(colorResId: Int) {
+        val drawable = background
+        if (drawable is LayerDrawable) {
+            val underline = drawable.findDrawableByLayerId(R.id.background) as? GradientDrawable
+            underline?.setColor(ContextCompat.getColor(context, colorResId))
+        } else {
+            background?.mutate()?.setTint(ContextCompat.getColor(context, colorResId))
+        }
     }
 }
