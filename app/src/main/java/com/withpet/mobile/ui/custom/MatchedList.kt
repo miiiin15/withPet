8e15d8fc5a991e8ca999abea0253ec0f08524c2f
mintae <gg04253@gmail.com>
2024-09-28 01:33:51 +0900

    Feat: MatchedList 추가 및 연결

---
+++ app/src/main/java/com/withpet/mobile/ui/custom/MatchedList.kt
@@ -0,0 +1,159 @@
+package com.withpet.mobile.ui.custom
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import com.withpet.mobile.R
+import com.withpet.mobile.data.model.Someone
+
+class MatchedList @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : RecyclerView(context, attrs) {
+
+    interface OnLikeRequestSuccess {
+        fun onLikeRequestSuccess(currentLike: Boolean, success: Boolean)
+    }
+
+    private var likeButtonClickListener: OnLikeRequestSuccess? = null
+    private val someoneAdapter = MatchedListAdapter()
+
+    init {
+        // GridLayoutManager를 사용하여 한 줄에 2개의 아이템 배치
+        layoutManager = GridLayoutManager(context, 2) // spanCount = 2
+        adapter = someoneAdapter
+    }
+
+    fun setSomeones(someones: List<Someone>) {
+        someoneAdapter.setSomeones(someones)
+    }
+
+    fun setOnItemClickListener(listener: (Someone) -> Unit) {
+        someoneAdapter.setOnItemClickListener(listener)
+    }
+
+    fun setOnLikeButtonClickListener(listener: OnLikeRequestSuccess) {
+        likeButtonClickListener = listener
+    }
+
+    inner class MatchedListAdapter :
+        RecyclerView.Adapter<MatchedListAdapter.MatchedListViewHolder>() {
+
+        private var someones: List<Someone> = emptyList()
+        private var itemClickListener: ((Someone) -> Unit)? = null
+
+        fun setSomeones(someones: List<Someone>) {
+            this.someones = someones
+            notifyDataSetChanged()
+        }
+
+        fun setOnItemClickListener(listener: (Someone) -> Unit) {
+            this.itemClickListener = listener
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchedListViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_matched_list, parent, false)
+            return MatchedListViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MatchedListViewHolder, position: Int) {
+            val someone = someones[position]
+            holder.bind(someone, position, someones.size)
+        }
+
+        override fun getItemCount(): Int = someones.size
+
+        inner class MatchedListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val cardView: MaterialCardView = itemView.findViewById(R.id.cardView)
+            private val addressText: TextView = itemView.findViewById(R.id.addressText)
+            private val usernameText: TextView = itemView.findViewById(R.id.usernameText)
+            private val ageText: TextView = itemView.findViewById(R.id.ageText)
+            private val actionButton: CustomLikeButton = itemView.findViewById(R.id.likeButton)
+            private val profileImage: ImageView = itemView.findViewById(R.id.profileImage)
+
+            init {
+                // 아이템의 기본 크기 설정 (화면의 40%로)
+                val displayMetrics = itemView.context.resources.displayMetrics
+                val screenWidth = displayMetrics.widthPixels
+                val itemSize = (screenWidth * 0.4).toInt() // 아이템 크기를 화면의 40%로 설정
+
+                // 아이템의 크기 및 기본 여백 설정
+                val layoutParams = cardView.layoutParams as ViewGroup.MarginLayoutParams
+                layoutParams.width = itemSize
+                layoutParams.height = itemSize
+                cardView.layoutParams = layoutParams
+            }
+
+            @SuppressLint("SetTextI18n")
+            fun bind(someone: Someone, position: Int, itemCount: Int) {
+                addressText.text = someone.regionName
+                usernameText.text = someone.nickName
+                ageText.text = "${someone.age}세"
+
+                // 화면 크기를 사용하여 정확한 여백 계산
+                val displayMetrics = itemView.context.resources.displayMetrics
+                val screenWidth = displayMetrics.widthPixels
+                val itemSize = (screenWidth * 0.4).toInt()
+                val remainingSpace = screenWidth - (itemSize * 2)
+                val itemMargin = remainingSpace / 3 // 각 아이템 간격을 동일하게 유지하기 위해 3으로 나눔
+
+                val layoutParams = cardView.layoutParams as ViewGroup.MarginLayoutParams
+                val addressTextLayoutParams =
+                    addressText.layoutParams as ViewGroup.MarginLayoutParams
+                val usernameTextLayoutParams =
+                    usernameText.layoutParams as ViewGroup.MarginLayoutParams
+
+                // 첫 번째 줄의 왼쪽 아이템 또는 첫 번째 아이템
+                if (position == 0) {
+                    layoutParams.leftMargin = itemMargin // 첫 아이템의 왼쪽 여백 설정
+                    layoutParams.rightMargin = itemMargin / 2 // 중간 아이템 간격은 절반으로 설정
+
+                    addressTextLayoutParams.leftMargin = itemMargin
+                    usernameTextLayoutParams.leftMargin = itemMargin
+                }
+                // 짝수번째 아이템 (오른쪽 아이템)
+                else if ((position + 1) % 2 == 0) {
+                    layoutParams.leftMargin = itemMargin / 2 // 짝수 아이템은 왼쪽 간격을 절반으로 설정
+                    layoutParams.rightMargin = itemMargin // 오른쪽 여백을 일반적으로 설정
+
+                    addressTextLayoutParams.leftMargin = itemMargin / 2
+                    usernameTextLayoutParams.leftMargin = itemMargin / 2
+                }
+                // 홀수번째 아이템 (왼쪽 아이템)
+                else {
+                    layoutParams.leftMargin = itemMargin // 홀수 아이템은 왼쪽 여백 설정
+                    layoutParams.rightMargin = itemMargin / 2 // 중간 아이템의 오른쪽 간격을 절반으로 설정
+
+                    addressTextLayoutParams.leftMargin = itemMargin
+                    usernameTextLayoutParams.leftMargin = itemMargin
+                }
+
+                // 마지막 줄의 오른쪽 아이템이면 오른쪽 여백 조정
+                if (position == itemCount - 1 && (position + 1) % 2 == 0) {
+                    layoutParams.rightMargin = itemMargin // 마지막 아이템 오른쪽 여백 설정
+                }
+
+                cardView.layoutParams = layoutParams
+
+                actionButton.setOnClickListener {
+                    likeButtonClickListener?.onLikeRequestSuccess(actionButton.isLike, true)
+                }
+
+                cardView.setOnClickListener {
+                    itemClickListener?.invoke(someone)
+                }
+            }
+        }
+
+
+    }
+}
--- app/src/main/java/com/withpet/mobile/ui/fragment/match/MatchFragment.kt
+++ app/src/main/java/com/withpet/mobile/ui/fragment/match/MatchFragment.kt
@@ -5,14 +5,96 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.withpet.mobile.BaseActivity
 import com.withpet.mobile.R
+import com.withpet.mobile.data.api.response.MemberInfo
+import com.withpet.mobile.data.model.Someone
+import com.withpet.mobile.data.session.UserSession
+import com.withpet.mobile.databinding.FragmentMatchBinding
+import com.withpet.mobile.ui.activity.main.SomeoneList
+import com.withpet.mobile.ui.custom.MatchedList
+import com.withpet.mobile.ui.custom.SomeoneInfoBottomSheet
+import com.withpet.mobile.viewmodel.MainViewModel
 
 class MatchFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentMatchBinding? = null
+
+    private val binding get() = _binding!!
+    private lateinit var userInfo: MemberInfo
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // 매칭 프래그먼트 레이아웃 인플레이트
-        return inflater.inflate(R.layout.fragment_match, container, false)
+        _binding = FragmentMatchBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.matchedList.observe(viewLifecycleOwner) { response ->
+            response?.payload?.let { someones ->
+                val someoneList: MatchedList = binding.matchedList
+                someoneList.setSomeones(someones) // 데이터 설정
+                someoneList.setOnItemClickListener { someone ->
+                    userInfo = UserSession.getUserInfo()
+
+                    UserSession.checkRegionInfo(
+                        onAvailable = { regionInfo ->
+                            showSomeoneInfoBottomSheet(someone)
+                        },
+                        onUnavailable = {
+                            (activity as? BaseActivity)?.showRegionPopup()
+                        }
+                    )
+                }
+                // Like 버튼 클릭 리스너 설정
+                someoneList.setOnLikeButtonClickListener(object :
+                    MatchedList.OnLikeRequestSuccess {
+                    override fun onLikeRequestSuccess(currentLike: Boolean, success: Boolean) {
+                        viewModel.handleLikeRequest(currentLike, success)
+                    }
+                }
+                )
+            }
+
+        }
+// 벨 아이콘 클릭 리스너 설정
+        binding.ivBellIcon.setOnClickListener {
+            // TODO : 아이콘 클릭 시 동작할 코드 작성
+            (activity as? BaseActivity)?.showAlert("알람 개발 중")
+        }
+
+        binding.ivHearthIcon.setOnClickListener {
+            // TODO : 아이콘 클릭 시 동작할 코드 작성
+            (activity as? BaseActivity)?.showAlert("좋아요 목록 개발 중")
+        }
+
+        // onCreateView에서 데이터가 이미 로드되지 않았는지 확인
+        if (!viewModel.isDataLoaded) {
+            viewModel.fetchMatchedList()
+        }
+
+        return binding.root
+    }
+
+    private fun showSomeoneInfoBottomSheet(someone: Someone) {
+        // BottomSheet를 표시하는 코드
+        val bottomSheet = SomeoneInfoBottomSheet()
+
+        // Someone 객체의 데이터를 BottomSheet에 설정
+//        bottomSheet.setProfileImage(someone.profileImage) // profileImage는 Someone 객체에 있는 필드
+        bottomSheet.setData(someone)
+
+
+        bottomSheet.setOnGreetClickListener {
+            // 인사하기 버튼 클릭 시 동작 설정
+        }
+
+        bottomSheet.setOnLikeClickListener {
+            // 좋아요 버튼 클릭 시 동작 설정
+        }
+
+        bottomSheet.show(fragmentManager!!, bottomSheet.tag)
     }
 }
