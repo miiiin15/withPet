63ba4daafbbc79ace3bf9f3101719b1524f1a2e9
mintae <gg04253@gmail.com>
2024-10-13 19:13:58 +0900

    Feat: 좋아요 삭제 기능 구현

--- app/src/main/java/com/withpet/mobile/ui/custom/LikedList.kt
+++ app/src/main/java/com/withpet/mobile/ui/custom/LikedList.kt
@@ -12,18 +12,23 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.withpet.mobile.R
 import com.withpet.mobile.data.model.Someone
+import com.withpet.mobile.data.repository.CommonRepo
+import com.withpet.mobile.utils.Constants
+import java.lang.Exception
 
 class LikedList @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : RecyclerView(context, attrs) {
 
-    interface OnLikeRequestSuccess {
-        fun onLikeRequestSuccess(currentLike: Boolean, success: Boolean)
+    interface OnDeleteRequestSuccess {
+        fun onDeleteRequestSuccess(success: Boolean)
     }
-    private var likeButtonClickListener: OnLikeRequestSuccess? = null
+
+    private var deleteButtonClickListener: OnDeleteRequestSuccess? = null
     private var viewHolder: LikedListAdapter.LikedListViewHolder? = null
     private val someoneAdapter = LikedListAdapter()
 
@@ -45,8 +50,8 @@ class LikedList @JvmOverloads constructor(
     }
 
     // 클릭 리스너를 외부에서 설정할 수 있는 메서드
-    fun setOnLikeButtonClickListener(listener: OnLikeRequestSuccess) {
-        likeButtonClickListener = listener
+    fun setOnDeleteButtonClickListener(listener: OnDeleteRequestSuccess) {
+        deleteButtonClickListener = listener
     }
 
     // 어댑터 클래스 정의
@@ -60,13 +65,17 @@ class LikedList @JvmOverloads constructor(
             notifyDataSetChanged()
         }
 
+        private fun deleteSomeone(memberId: Int) {
+            setSomeones(this.someones.filter { it.memberId != memberId })
+        }
+
         fun setOnItemClickListener(listener: (Someone) -> Unit) {
             this.itemClickListener = listener
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikedListViewHolder {
             val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_someone_list, parent, false)
+                .inflate(R.layout.item_liked_list, parent, false)
             return LikedListViewHolder(view)
         }
 
@@ -78,12 +87,13 @@ class LikedList @JvmOverloads constructor(
         override fun getItemCount(): Int = someones.size
 
         inner class LikedListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private val itemView: LinearLayout = itemView.findViewById(R.id.likedItemView)
+            private val infoView: LinearLayout = itemView.findViewById(R.id.likedInfoView)
             private val addressText: TextView = itemView.findViewById(R.id.liked_addressText)
             private val usernameText: TextView = itemView.findViewById(R.id.liked_usernameText)
             private val ageText: TextView = itemView.findViewById(R.id.liked_ageText)
             private val profileImage: ImageView =
                 itemView.findViewById(R.id.liked_profileImage) // 프로필 이미지뷰
+            private val deleteButton: CustomButton = itemView.findViewById(R.id.liked_deleteButton)
 
 
             init {
@@ -95,21 +105,45 @@ class LikedList @JvmOverloads constructor(
 
                 addressText.text = someone.regionName
                 usernameText.text = someone.nickName
-                ageText.text = "${someone.age}세"
+                ageText.text = "・${someone.age}세"
 
-                // TODO: 프로필 이미지 설정
-                // 예: Glide.with(itemView).load(someone.profileImage).into(profileImage)
+                val fullImageUrl =
+                    Constants.IMAGE_URL + "media" + someone.profileImage?.replace("\\", "/")
+                Glide.with(itemView).load(fullImageUrl).into(profileImage)
 
-                // TODO : 삭제 버튼 리스너
-//                deleteButton.setOnClickListener {
-//                }
-
-
-
-                // 카드뷰 클릭 리스너 설정
-                itemView.setOnClickListener {
+                infoView.setOnClickListener {
                     itemClickListener?.invoke(someone)
                 }
+
+                deleteButton.setOnClickListener {
+                    requestDislike(someone.memberId.toString()) {
+                        deleteButtonClickListener?.onDeleteRequestSuccess(it)
+                    }
+                }
+
+            }
+
+        }
+
+        fun requestDislike(memberId: String, requestSuccess: (isSuccess: Boolean) -> Unit?) {
+            try {
+                CommonRepo.requestDislike(
+                    memberId,
+                    success = {
+                        requestSuccess(true)
+                        deleteSomeone(memberId.toInt())
+                    },
+                    failure = {
+                        requestSuccess(false)
+                    },
+                    networkFail = {
+                        requestSuccess(false)
+                    }
+                )
+            } catch (e: Exception) {
+
+            } finally {
+                // 항상 실행되는 블록 (필요한 경우)
             }
 
         }
