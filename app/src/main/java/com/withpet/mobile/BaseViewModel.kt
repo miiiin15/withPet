3b595802ff9712f1ca4da4fe8fdcb9b02d9dfc96
mintae <gg04253@gmail.com>
2024-11-03 18:07:30 +0900

    Feat: BaseViewModel 추가

---
+++ app/src/main/java/com/withpet/mobile/BaseViewModel.kt
@@ -0,0 +1,35 @@
+package com.withpet.mobile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    protected val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    protected val _failure = MutableLiveData<Throwable>()
+    val failure: LiveData<Throwable> get() = _failure
+
+    abstract fun fetchData(): Job
+
+    protected fun launchDataLoad(block: suspend () -> Unit): Job {
+        _isLoading.value = true
+        return viewModelScope.launch {
+            try {
+                block()
+            } catch (e: Exception) {
+                _error.value = e.message
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+}
