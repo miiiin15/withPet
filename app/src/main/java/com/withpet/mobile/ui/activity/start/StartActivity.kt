9472b396b52636fddf72a48e9a81f6c85eae6e47
mintae <gg04253@gmail.com>
2024-11-24 13:01:39 +0900

    Test: 테스트용 엑티비티 삭제

--- app/src/main/java/com/withpet/mobile/ui/activity/start/StartActivity.kt
+++ app/src/main/java/com/withpet/mobile/ui/activity/start/StartActivity.kt
@@ -6,7 +6,6 @@ import com.withpet.mobile.BaseActivity
 import com.withpet.mobile.databinding.ActivityStartBinding
 import com.withpet.mobile.ui.activity.login.LoginActivity
 import com.withpet.mobile.ui.activity.signup.SignupActivity
-import com.withpet.mobile.ui.test.TestActivity
 
 class StartActivity : BaseActivity() {
     private lateinit var binding: ActivityStartBinding
@@ -16,7 +15,6 @@ class StartActivity : BaseActivity() {
 
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setupClickListener()
     }
 
@@ -29,21 +27,18 @@ class StartActivity : BaseActivity() {
             handleButtonClick(ButtonType.REGISTER)
         }
 
-        binding.imgLogoName.setOnClickListener {
-            handleButtonClick(ButtonType.LOGO)
-        }
+
     }
 
     private fun handleButtonClick(buttonType: ButtonType) {
         val intent = when (buttonType) {
             ButtonType.START -> Intent(this, LoginActivity::class.java)
             ButtonType.REGISTER -> Intent(this, SignupActivity::class.java)
-            ButtonType.LOGO -> Intent(this, TestActivity::class.java)
         }
         startActivity(intent)
     }
 
     private enum class ButtonType {
-        START, REGISTER, LOGO
+        START, REGISTER
     }
 }
