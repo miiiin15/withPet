57b236de74e05db1f69be887365cf09e52af677a
mintae <gg04253@gmail.com>
2024-12-16 12:33:28 +0900

    feat: EncryptedSharedPreferences 암호화 적용

--- app/src/main/java/com/withpet/mobile/data/api/TokenRepository.kt
+++ app/src/main/java/com/withpet/mobile/data/api/TokenRepository.kt
@@ -1,9 +1,21 @@
 package com.withpet.mobile.data.api
 
 import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 
-class TokenRepository(private val context: Context) {
-    private val preferences = context.getSharedPreferences("dataPreferences", Context.MODE_PRIVATE)
+class TokenRepository(context: Context) {
+    private val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val preferences = EncryptedSharedPreferences.create(
+        context,
+        "encryptedDataPreferences",
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
 
     fun saveAccessToken(token: String) {
         preferences.edit().putString("access-token", token).apply()
